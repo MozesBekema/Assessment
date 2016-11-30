9be097ef226beabf45982e68eee6cd5dfd1bd499
joostdelange <post@joostdelange.nl>
2016-11-30 09:21:29 +0100

    adzf

--- inc/opleiding.php
+++ inc/opleiding.php
@@ -18,20 +18,22 @@
     <header>
         <div class="demo-card-wide mdl-card mdl-shadow--2dp">
             <div class="mdl-card__supporting-text">
-                <form action="klassen.php" method="post" class="form">
-                    <select name="opleidingen">
-                        <?php
-                            while($row = $opleidingen->fetch(PDO::FETCH_ASSOC)){ 
-                        ?>
-                        <option value="<?php echo ($row['id']);?>">
-                            <?php echo ($row['opleiding_name']);?>
-                        </option>
-                        <?php
-                            }
-                        ?>
-                    </select>
-                    <?php include("button.php")?>
-                </form>
+                <div class="input-field col s12">
+                    <form action="klassen.php" method="post" class="form-group">
+                        <select name="opleidingen" class="turnintodropdown">
+                            <?php
+                                while($row = $opleidingen->fetch(PDO::FETCH_ASSOC)){
+                            ?>
+                            <option value="<?php echo ($row['id']);?>">
+                                <?php echo ($row['opleiding_name']);?>
+                            </option>
+                            <?php
+                                }
+                            ?>
+                        </select>
+                        <?php include("button.php")?>
+                    </form>
+                </div>
             </div>
         </div>
     </header>
--- inc/werkprocessen.php
+++ inc/werkprocessen.php
@@ -19,22 +19,22 @@
             <form action="leerling.php" method="post">
                 <thead>
                     <tr>
-                        <th class="mdl-data-table__cell--non-numeric">Kies de werkprocessen voor </th>
+                        <th class="mdl-data-table__cell--non-numeric">Kies de werkprocessen voor hallo</th>
                     </tr>
                 </thead>
             <tbody>
                 <?php
-                    while($row = $wp->fetch(PDO::FETCH_ASSOC)){ 
+                    while($row = $wp->fetch(PDO::FETCH_ASSOC)){
                 ?>
                 <tr>
-                    <td class="mdl-data-table__cell--non-numeric" name="<?php echo ($row['id']);?>"><?php echo ($row['wp_name']);?></td>
+                    <td class="mdl-data-table__cell--non-numeric" name="<?php echo ($row['id']);?>" required><?php echo ($row['wp_name']);?></td>
                 </tr>
                 <?php
                     }
                 ?>
             </tbody>
         </table>
-            <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Verzenden</button>
+            <?php include("button.php")?>
             </form>
     </header>
 </body>
