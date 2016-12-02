58c9290f1ec7b38e48c8de6b13095eb90286fba0
Thomas Bekema <mozes@Thomass-Mac.local>
2016-12-02 13:07:33 +0100

    hallo class

--- inc/menu.php
+++ inc/menu.php
@@ -3,7 +3,8 @@
 ?>
 <nav>
     <ul>
-        <?php echo "Hallo, ".$_SESSION['login_user']['name'] ?>
         <li><a href="../index.php?action=logout">Log out</a></li>
+        <li class="hallo"><?php echo "Hallo, ".$_SESSION['login_user']['name'] ?></li>
+
     </ul>
 </nav>
