9c89360818177af7d4f059ffff35529ba25f4c37
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-02 15:13:53 +0100

    Over en uit

--- inc/beoordelen.php
+++ inc/beoordelen.php
@@ -3,6 +3,10 @@
     include("logincheck.php");
 //    var_dump($_SESSION);
     include("connect.php");
+    $klas = $conn->prepare("SELECT klas_name, id FROM `klas` WHERE id = :id");
+    $klas->execute(array('id' => $_SESSION['klassen']));
+    $row = $klas->fetch(PDO::FETCH_ASSOC);
+//    var_dump($row['klas_name']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,12 +16,12 @@
     <header>
             <form action="confirm.php" method="post">
                 <?php
-                    echo "Je bent <b>".$_POST['Leerling']."</b> aan het beoordelen, uit klas ".$_SESSION[''];
+                    echo "Je bent <b>".$_POST['Leerling']."</b> aan het beoordelen, uit klas ".$row['klas_name'];
                 ?>
                 <?php
 
                 ?>
-                <input type="range" min="1" max="5" step="1" name="<?php echo ($row['wp_name']);?>">
+                <input type="range" min="1" max="5" step="1" name="<?php echo ($row['']);?>">
                 <?php
 
                 ?>
