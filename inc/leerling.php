886f52700647ae35ba41d7cc3d79acaa8a41cb7d
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-09 11:48:27 +0100

    Update

--- inc/leerling.php
+++ inc/leerling.php
@@ -1,9 +1,10 @@
 <?php
     include("logincheck.php");
+var_dump($_SESSION);
     include("connect.php");
 
-    $leerling = $conn->prepare("SELECT name, id FROM `leerlingen` ORDER BY name");
-    $leerling->execute();
+    $leerling = $conn->prepare("SELECT name, id, klas_id FROM `leerlingen` WHERE klas_id = :klas_id");
+    $leerling->execute(array('klas_id' => $_SESSION['klassen']));
 
     $_SESSION['werkprocessen'] = array();
     // loop over werkprocessen
