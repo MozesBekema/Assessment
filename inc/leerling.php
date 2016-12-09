1562912db3cd47fb76177dc3ef7bf249216a8090
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-09 11:15:06 +0100

    Update

--- inc/leerling.php
+++ inc/leerling.php
@@ -20,6 +20,9 @@
     <body>
         <?php include("menu.php"); ?>
         <header>
+            <div class="card-wide mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title">
+                </div>
                 <form action="beoordelen.php" method="post">
                     <select name="Leerling">
                         <?php
