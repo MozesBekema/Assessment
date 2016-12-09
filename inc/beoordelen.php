4ceab72dd9ea605d18be0e99b79ed046b242a8fa
Joost de Lange <joostdelange@Joosts-Mac.local>
2016-12-09 13:07:12 +0100

    Update

--- inc/beoordelen.php
+++ inc/beoordelen.php
@@ -15,7 +15,8 @@
     <?php include("menu.php"); ?>
     <header>
         <div class="card-wide mdl-card mdl-shadow--2dp">
-            <div class="mdl-card__title"><h2>Beoordelen.</h2>
+            <div class="mdl-card__title">
+                <h2>Beoordelen.</h2>
             </div>
             <form action="confirm.php" method="post">
                 <?php
