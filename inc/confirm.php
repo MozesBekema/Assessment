9a0ada3add861e9b0e2630d7dfda2764434889e1
joostdelange <post@joostdelange.nl>
2016-12-16 15:10:39 +0100

    Update

--- inc/confirm.php
+++ inc/confirm.php
@@ -21,7 +21,9 @@
                 </div>
             <form action="" method="post">
                 <?php echo "U heeft <b>".$_SESSION['leerling']."</b> beoordeeld, wilt u dit versturen?" ?><br/></br>
-                <?php echo $_SESSION['leerling'] ?><br/></br>
+                <?php foreach ($_SESSION['beoordeling'] as $key => $value){ ?>
+                    <?php echo ($key . ' ' . $value); ?><br/></br>
+                <?php } ?>
                 <button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Verstuur</button>
             </form>
         </div>
---
+++ inc/opties.php
@@ -0,0 +1,23 @@
+<?php
+    include("logincheck.php");
+    include("connect.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+    <?php include("head.php"); ?>
+    <body>
+        <?php include("menu.php"); ?>
+        <header>
+            <div class="card-wide mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title">
+                    <h2>Opties</h2>
+                </div>
+            </div>
+        </header>
+    </body>
+</html>
